8cb86e4c1b56d774adc8c27d32035bf3d2a39006
François Lagarde <f.lagarde@ipnl.in2p3.fr>
2017-02-28 21:53:35 +0100

    Show example

--- src/main.cpp
+++ src/main.cpp
@@ -14,12 +14,17 @@ int main(int argc, char *argv[])
   n[4]= new THistoGraph<TProfile>("test2","test",3,0,3);
   n[5]= new THistoGraph<TProfile2D>("test2","test",3,0,3,5,0,5);
   n[6]= new THistoGraph<TProfile3D>("test2","test",3,0,3,5,0,5,6,0,6);
+  n[7]= new THistoGraph<TGraph>("test2","test");
+  n[8]= new THistoGraph<TGraphAuto>("MonGraphAutoCheri","MonGraphAuto");
   n[1]->Fill(1,2.0);
   n[2]->Fill(1.0,2.0,3.0);
   n[3]->Fill(1.0,2.0,3.0);
   n[4]->Fill(1,2.0,3.0);
   n[5]->Fill(1.0,2.0,3.0,4.5);
   n[6]->Fill(1.0,2.0,3.0,6.5);
+  n[8]->Fill(1,2);
+  n[8]->Fill(3,4);
+  n[8]->Fill(10,15);
   for(std::map<int,HistoGraphBase*>::iterator it=n.begin();it!=n.end();++it)
   {
    it->second->Write();
